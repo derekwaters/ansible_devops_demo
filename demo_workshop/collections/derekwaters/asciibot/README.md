34f004936d289f26947055a313d86d822cd23422
Derek <derek@frisbeeworld.com>
2026-07-16 17:08:26 +1000

    Add sample exercise code

---
+++ demo_workshop/collections/derekwaters/asciibot/README.md
@@ -0,0 +1,3 @@
+# Ansible Collection - derekwaters.asciibot
+
+This collection contains a bot that only likes to speak in ASCII art.
---
+++ demo_workshop/collections/derekwaters/asciibot/plugins/modules/hello.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+DOCUMENTATION = r'''
+---
+module: hello
+short_description: Say hello, in a pretty way
+description:
+    - Say hello to someone in a pretty fashion
+version_added: "1.0.0"
+options:
+    account_name:
+        description: A username to greet
+        required: true
+        type: str
+'''
+
+EXAMPLES = r'''
+- name: Say hello to Joe
+  derekwaters.asciibot.hello:
+    account_name: Joe
+'''
+
+RETURN = r'''
+greeting:
+    description: The pretty greeting message
+    type: str
+    returned: always
+'''
+
+from art import text2art
+
+def main():
+    module_args = dict(
+        account_name = dict(type = 'str', required = True)
+    )
+    module = AnsibleModule(
+        argument_spec = module_args,
+        supports_check_mode = True
+    )
+
+    name = module.params['account_name']
+
+    msg = text2art("Hi " + name)
+
+    module.exit_json(greeting=msg)
+
+if __name__ == '__main__':
+    main()
